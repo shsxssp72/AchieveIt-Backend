7e14106b990927b458c4e58384158c4091d12312
shsxssp72 <shsxssp72@hotmail.com>
2020-03-10 22:34:21 +0800

    Enable project issue controller

--- achieveit_project/src/main/java/com/april/achieveit_project/controller/ProjectIssueController.java
+++ achieveit_project/src/main/java/com/april/achieveit_project/controller/ProjectIssueController.java
@@ -1,8 +1,11 @@
 package com.april.achieveit_project.controller;
 
 import com.april.achieveit_common.bean.ResponseContent;
+import com.april.achieveit_common.bean.ResponseContentStatus;
+import com.april.achieveit_project.service.ProjectService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -14,16 +17,35 @@ public class ProjectIssueController
     //TODO Placeholder
 
     private static Logger logger=LoggerFactory.getLogger(ProjectIssueController.class);
+    @Autowired
+    ProjectService projectService;
 
     @GetMapping(path="/{project_id}")
     public ResponseContent GetProjectIssueTrackerUrl(@PathVariable(name="project_id") String projectId)
     {
-        return null;
+        logger.info("Invoking :"+Thread.currentThread()
+                .getStackTrace()[1].getMethodName());
+        ResponseContent result=new ResponseContent();
+
+        String issueTrackerUrl=projectService.SelectMiscByProjectIdAndKey(projectId,
+                                                                          "issue_tracker_url");
+        result.setResult(issueTrackerUrl);
+        result.setStatus(ResponseContentStatus.SUCCESS);
+        return result;
     }
 
     @PostMapping(path="/{project_id}")
     public ResponseContent GetProjectIssueTrackerUrl(@PathVariable(name="project_id") String projectId,@RequestBody Map<String,String> params)
     {
-        return null;
+        logger.info("Invoking :"+Thread.currentThread()
+                .getStackTrace()[1].getMethodName());
+        ResponseContent result=new ResponseContent();
+
+        String issueTrackerUrl=params.get("issue_tracker_url");
+        projectService.InsertMiscByProjectIdAndKey(projectId,
+                                                   "issue_tracker_url",
+                                                   issueTrackerUrl);
+        result.setStatus(ResponseContentStatus.SUCCESS);
+        return result;
     }
 }
