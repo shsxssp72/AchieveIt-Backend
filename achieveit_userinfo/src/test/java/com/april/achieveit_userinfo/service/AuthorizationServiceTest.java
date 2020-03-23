27f4860230e096102c661621231facee8b1dcdbb
10165101247zyc <847483868@qq.com>
2020-03-24 00:09:13 +0800

    update 2 test files in user

--- achieveit_userinfo/src/test/java/com/april/achieveit_userinfo/service/AuthorizationServiceTest.java
+++ achieveit_userinfo/src/test/java/com/april/achieveit_userinfo/service/AuthorizationServiceTest.java
@@ -69,7 +69,8 @@ class AuthorizationServiceTest {
     @Test
     void getUserPermissionName() {
         List<String> list=authorizationService.GetUserPermissionName("2020-4577-D-02","SYKJ-20200201-0001");
-        Assert.assertThat(list.get(0),is("许可3"));
+        System.out.println(list);
+        Assert.assertThat(list.get(0),is("issue_tracker_access"));
     }
 
     //project_role_id_list=[{"project_role_id":"123","superior_id":"SYKJ-20200201-0010","project_role_name":"角色1"}]
@@ -92,18 +93,25 @@ class AuthorizationServiceTest {
     void updateProjectMember() {
         Map<String,String> map=new HashMap<>();
         map.put("user_id","SYKJ-20200201-0004");
-        map.put("project_role_id_list","[{\"project_role_id\":\"123\",\"superior_id\":\"SYKJ-20200201-0010\",\"project_role_name\":\"角色4\"}]");
+        map.put("project_role_id_list","[{\"project_role_id\":\"126\",\"superior_id\":\"SYKJ-20200201-0010\",\"project_role_name\":\"角色4\"}]");
         List<Map<String,String>> members = new ArrayList<>();
         members.add(map);
         authorizationService.UpdateProjectMember("2020-4577-D-02",members);
+        Assert.assertThat(authorizationService.GetProjectMember("2020-4577-D-02").get(0).get("project_role_name"),is("角色4"));
 
     }
 
+    @Transactional
     @Test
     void updateUserProjectPermission() {
+        List<String> list = new ArrayList<>();
+        list.add("working_hour_access");
+        authorizationService.UpdateUserProjectPermission("2020-4577-D-02","SYKJ-20200201-0001",list);
+
     }
 
     @Test
     void getEditablePermissions() {
+        System.out.println(authorizationService.getEditablePermissions());
     }
 }
