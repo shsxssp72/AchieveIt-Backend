bb468bea42222c973ecc0950fe5015a45b1ff525
10165101247zyc <847483868@qq.com>
2020-03-24 16:38:56 +0800

    3.24 fix in user

--- achieveit_userinfo/src/test/java/com/april/achieveit_userinfo/service/AuthenticationServiceTest.java
+++ achieveit_userinfo/src/test/java/com/april/achieveit_userinfo/service/AuthenticationServiceTest.java
@@ -1,7 +1,14 @@
 package com.april.achieveit_userinfo.service;
 
+import com.april.achieveit_userinfo.mapper.UserInfoMapper;
+import com.april.achieveit_userinfo_interface.entity.UserInfo;
+
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,22 +22,34 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class AuthenticationServiceTest {
+    @Autowired
+    AuthenticationService authenticationService;
+    @Autowired
+    UserInfoMapper userInfoMapper;
+    @BeforeAll
+    public void setUp() throws Exception{
+        System.out.println("setup");
+        UserInfo userInfo = new UserInfo("SYKJ-20200201-0001",287991808128974852L,"mpan@hotmail.com","466071c6546622410622052f2b285dd47e933021dfe1e9576a4d8bfa2abe85f20bdbe269a306e50d5bc671ea19bbc1e317abfa0539b2d148dd875ca6de0be645","nrRPQXyABflAbUlJOlRGByAZHrjgxspLwrqyRmSnFCOuOcGaCjpGXcxAdeJfNgio",290104426675306497L);
+        userInfoMapper.insert(userInfo);
+    }
+    //密码123123
 
-    @Autowired AuthenticationService authenticationService;
     @Test
     void queryByUserId() {
-        Assert.assertThat(authenticationService.queryByUserId("SYKJ-20200201-0001").getUserName(),is("阿萨德"));
+        Assert.assertThat(authenticationService.queryByUserId("SYKJ-20200201-0001").getUserName(),is("mpan@hotmail.com"));
     }
 
     @Test
     void verifyIdentity() {
-         Assert.assertTrue(authenticationService.VerifyIdentity("SYKJ-20200201-0001","321321"));
+         Assert.assertTrue(authenticationService.VerifyIdentity("SYKJ-20200201-0001","123123"));
     }
 
     @Test
     void login() {
-        Assert.assertThat(authenticationService.Login("阿萨德","321321").substring(0,10),is("eyJhbGciOi"));
+        //System.out.println(authenticationService.Login("mpan@hotmail.com","123123"));
+        Assert.assertThat(authenticationService.Login("mpan@hotmail.com","123123").substring(0,10),is("eyJhbGciOi"));
     }
 
     @Test
@@ -40,7 +59,14 @@ class AuthenticationServiceTest {
     @Transactional
     @Test
     void updateUserInfo() {
-        authenticationService.UpdateUserInfo("SYKJ-20200201-0001","321321","123123");
-        Assert.assertTrue(authenticationService.VerifyIdentity("SYKJ-20200201-0001","123123"));
+        authenticationService.UpdateUserInfo("SYKJ-20200201-0001","123123","321321");
+       Assert.assertTrue(authenticationService.VerifyIdentity("SYKJ-20200201-0001","321321"));
     }
+
+    @AfterAll
+    public  void tearDownAfterClass() throws Exception{
+    userInfoMapper.deleteByPrimaryKey("SYKJ-20200201-0001");
+    }
+
+
 }
--- achieveit_userinfo/src/test/java/com/april/achieveit_userinfo/service/AuthorizationServiceTest.java
+++ achieveit_userinfo/src/test/java/com/april/achieveit_userinfo/service/AuthorizationServiceTest.java
@@ -1,7 +1,15 @@
 package com.april.achieveit_userinfo.service;
 
+import com.april.achieveit_userinfo.mapper.ProjectRoleMapper;
+import com.april.achieveit_userinfo.mapper.ProjectUserRelationMapper;
+import com.april.achieveit_userinfo.mapper.UserInfoMapper;
+import com.april.achieveit_userinfo_interface.entity.ProjectRole;
+import com.april.achieveit_userinfo_interface.entity.ProjectUserRelation;
+import com.april.achieveit_userinfo_interface.entity.UserInfo;
 import org.junit.Assert;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,44 +26,72 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class AuthorizationServiceTest {
     @Autowired AuthorizationService authorizationService;
+    @Autowired
+    ProjectUserRelationMapper projectUserRelationMapper;
+    @Autowired
+    ProjectRoleMapper projectRoleMapper;
+    @Autowired
+    UserInfoMapper userInfoMapper;
+
+    @BeforeAll
+    public void setup(){
+//        ProjectUserRelation projectUserRelation1 = new ProjectUserRelation("2020-4577-D-01","SYKJ-20200201-0000","SYKJ-20200101-0000",290089467161608193L);
+//        ProjectUserRelation projectUserRelation2 = new ProjectUserRelation("2020-4577-D-02","SYKJ-20200201-0001","SYKJ-20200101-0010",290089467161608123L);
+//        ProjectUserRelation projectUserRelation3 = new ProjectUserRelation("2020-4577-D-02","SYKJ-20200201-0003","SYKJ-20200101-0010",290089467161608125L);
+//
+//        projectUserRelationMapper.insert(projectUserRelation1);
+//        projectUserRelationMapper.insert(projectUserRelation2);
+//        projectUserRelationMapper.insert(projectUserRelation3);
+//        ProjectRole projectRole1 = new ProjectRole(290089467161608123L,"QaLeader");
+//        ProjectRole projectRole2 = new ProjectRole(290089467161608125L,"EPG");
+//        projectRoleMapper.insert(projectRole1);
+//        projectRoleMapper.insert(projectRole2);
+//        UserInfo userInfo = new UserInfo("SYKJ-20200201-0001",287991808128974852L,"mpan@hotmail.com","466071c6546622410622052f2b285dd47e933021dfe1e9576a4d8bfa2abe85f20bdbe269a306e50d5bc671ea19bbc1e317abfa0539b2d148dd875ca6de0be645","nrRPQXyABflAbUlJOlRGByAZHrjgxspLwrqyRmSnFCOuOcGaCjpGXcxAdeJfNgio",290104426675306497L);
+//        userInfoMapper.insert(userInfo);
+
+
+        System.out.println("setup");
+
+    }
 
     @Test
     void getUserProjectRole() {
-        Assert.assertThat(authorizationService.GetUserProjectRole("2020-4577-D-02","SYKJ-20200201-0001").get("project_id"),is("2020-4577-D-02"));
+        Assert.assertThat(authorizationService.GetUserProjectRole("2020-4577-D-01","SYKJ-20200201-0000").get("project_id"),is("2020-4577-D-01"));
 
     }
 
     @Test
     void getUserRoleFromMultipleProject() {
-        List<Map<String,String>> list =authorizationService.GetUserRoleFromMultipleProject("SYKJ-20200201-0001");
-        Assert.assertThat(list.get(0).get("project_id"),is("2020-4577-D-02"));
+        List<Map<String,String>> list =authorizationService.GetUserRoleFromMultipleProject("SYKJ-20200201-0000");
+        Assert.assertThat(list.get(0).get("project_id"),is("2020-4577-D-01"));
     }
 
     @Test
     void batchGetUserRoleFromMultipleProject() {
         List<String> list = new ArrayList<>();
+        list.add("SYKJ-20200201-0000");
         list.add("SYKJ-20200201-0001");
-        list.add("SYKJ-20200201-0002");
         List<Map<String,String>> resultList =authorizationService.BatchGetUserRoleFromMultipleProject(list);
         System.out.println(authorizationService.BatchGetUserRoleFromMultipleProject(list));
-        Assert.assertThat(resultList.get(0).get("project_id"),is("2020-4577-D-02"));
-        Assert.assertThat(resultList.get(1).get("project_id"),is("2020-4577-D-03"));
+        Assert.assertThat(resultList.get(0).get("project_id"),is("2020-4577-D-01"));
+        Assert.assertThat(resultList.get(1).get("project_id"),is("2020-4577-D-02"));
     }
 
     @Test
     void getProjectMember() {
         List<Map<String,String>> list =authorizationService.GetProjectMember("2020-4577-D-02");
         System.out.println(list);
-        Assert.assertThat(list.get(0).get("project_role_name"),is("角色1"));
-        Assert.assertThat(list.get(1).get("project_role_name"),is("角色3"));
+        Assert.assertThat(list.get(0).get("project_role_name"),is("QaLeader"));
+        Assert.assertThat(list.get(1).get("project_role_name"),is("EPG"));
     }
 
     @Test
     void getUserGlobalRole() {
         Map<String,String> map=authorizationService.GetUserGlobalRole("SYKJ-20200201-0001");
-        Assert.assertThat(map.get("global_role_name"),is("全局1"));
+        Assert.assertThat(map.get("global_role_name"),is("ConfigurationManager"));
     }
 
     @Test
@@ -74,17 +110,17 @@ class AuthorizationServiceTest {
     }
 
     //project_role_id_list=[{"project_role_id":"123","superior_id":"SYKJ-20200201-0010","project_role_name":"角色1"}]
-    @Transactional
+
     @Test
     void updateUserProjectRole() {
         Map<String,String> map=new HashMap<>();
-        map.put("project_role_name","角色5");
+        map.put("project_role_name","EPG");
         map.put("superior_id","SYKJ-20200201-0010");
-        map.put("project_role_id","123");
+        map.put("project_role_id","290089467161608193");
 
         List<Map<String,String>> list =new ArrayList<>();
         list.add(map);
-       authorizationService.UpdateUserProjectRole("2020-4577-D-02","SYKJ-20200201-0001",list);
+       authorizationService.UpdateUserProjectRole("2020-4577-D-01","SYKJ-20200201-0000",list);
        //Assert.assertThat(authorizationService.GetUserProjectRole("2020-4577-D-02",""));
     }
 
@@ -106,7 +142,7 @@ class AuthorizationServiceTest {
     void updateUserProjectPermission() {
         List<String> list = new ArrayList<>();
         list.add("working_hour_access");
-        authorizationService.UpdateUserProjectPermission("2020-4577-D-02","SYKJ-20200201-0001",list);
+        authorizationService.UpdateUserProjectPermission("2020-4577-D-01","SYKJ-20200201-0000",list);
 
     }
 
