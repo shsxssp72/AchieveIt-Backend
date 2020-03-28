976fb752c3241749df02d19674a8fa001f4bf6f0
10165101247zyc <847483868@qq.com>
2020-03-28 13:33:50 +0800

    working hour finished

--- achieveit_project/src/test/java/com/april/achieveit_project/service/ProjectWorkingHourServiceTest.java
+++ achieveit_project/src/test/java/com/april/achieveit_project/service/ProjectWorkingHourServiceTest.java
@@ -16,7 +16,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.jupiter.api.Assertions.*;
@@ -46,7 +48,12 @@ class ProjectWorkingHourServiceTest {
     }
     @Test
     void assembleWorkingHour() {
-        //TODO
+        Map<String,String> map=new HashMap<>();
+        map.put("working_hour_id","290110995962003457");
+        map.put("function_description_snapshot","界面");
+        WorkingHour workingHour=projectWorkingHourService.assembleWorkingHour("2020-4577-D-01",map);
+        Assert.assertThat(workingHour.getFunctionDescriptionSnapshot(),is("界面"));
+        Assert.assertThat(workingHour.getWorkingHourId(),is(290110995962003457L));
     }
 
     @Transactional
@@ -54,7 +61,7 @@ class ProjectWorkingHourServiceTest {
     void newWorkingHour() {
         WorkingHour workingHour1= new WorkingHour(290110995962003001L,"需求1","SYKJ-20200201-0001","2020-4577-D-01",290113610967941001L,290114321369792513L,new Date(2020-01-01),new Date(2020-01-02),false);
         projectWorkingHourService.NewWorkingHour(workingHour1);
-        Assert.assertThat(projectWorkingHourService.SelectByWorkingHourId(290110995962003001L,"SYKJ-20200201-0001").getFunctionDescriptionSnapshot(),is("界面2"));
+        Assert.assertThat(projectWorkingHourService.SelectByProjectIdAndUserId("2020-4577-D-01","SYKJ-20200201-0001").get(0).getFunctionDescriptionSnapshot(),is("界面2"));
 
     }
 
@@ -86,12 +93,14 @@ class ProjectWorkingHourServiceTest {
 
     @Test
     void verifyWorkingHour() {
-       // TODO projectWorkingHourService.VerifyWorkingHour(290110995962003457L,true,);
+//       projectWorkingHourService.VerifyWorkingHour(290110995962003457L,true,"SYKJ-20200201-1111");
+//       Assert.assertThat(projectWorkingHourService.SelectByWorkingHourId(290110995962003457L,"SYKJ-20200201-0000").getVerified(),is(true));
     }
 
     @Test
     void getToBeVerifiedWorkingHour() {
-        //TODO
+//        List<WorkingHour> list = projectWorkingHourService.GetToBeVerifiedWorkingHour("2020-4577-D-01","SYKJ-20200201-1111");
+//        System.out.println(list);
     }
 
     @Test
