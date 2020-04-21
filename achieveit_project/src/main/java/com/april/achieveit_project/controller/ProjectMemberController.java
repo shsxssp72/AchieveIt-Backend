acb30197612ef21cef7c09e270647f951bc62cb6
shsxssp72 <shsxssp72@hotmail.com>
2020-04-21 17:25:58 +0800

    Prevent finished project modify members

--- achieveit_project/src/main/java/com/april/achieveit_project/controller/ProjectMemberController.java
+++ achieveit_project/src/main/java/com/april/achieveit_project/controller/ProjectMemberController.java
@@ -5,8 +5,9 @@ import com.april.achieveit_common.bean.ResponseContentStatus;
 import com.april.achieveit_common.utility.CookieUtility;
 import com.april.achieveit_common.utility.JWTUtility;
 import com.april.achieveit_project.client.RoleServiceClient;
+import com.april.achieveit_project.config.ProjectStateTransition;
+import com.april.achieveit_project.entity.Project;
 import com.april.achieveit_project.service.ProjectService;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 import org.slf4j.Logger;
@@ -36,18 +37,30 @@ public class ProjectMemberController
     {
         logger.info("Invoking :"+Thread.currentThread()
                 .getStackTrace()[1].getMethodName());
+        Project currentProject=projectService.SelectByProjectId(projectId);
+        if(ProjectStateTransition.ProjectState.Finished.name()
+                .equals(currentProject.getStatus())||ProjectStateTransition.ProjectState.ReadyArchive.name()
+                .equals(currentProject.getStatus())||ProjectStateTransition.ProjectState.Archived.name()
+                .equals(currentProject.getStatus())||ProjectStateTransition.ProjectState.ArchiveDeclined.name()
+                .equals(currentProject.getStatus()))
+        {
+            throw new IllegalArgumentException("Finished project cannot modify member.");
+        }
+
         params.put("project_id",
                    projectId);
         var updateResult=roleServiceClient.UpdateProjectMember(params);
 
         if(updateResult.getStatus()==ResponseContentStatus.SUCCESS)
         {
-            String jwt=CookieUtility.getCookieValue(request,"JWT");
+            String jwt=CookieUtility.getCookieValue(request,
+                                                    "JWT");
             String userId=JWTUtility.getSubjectFromJWT(jwt);
 
             //When QA Manager, EPG Leader, Conf. Manager add members. allow PM to push project forward.
             String global_role_name=projectService.getUserGlobalRole(userId);
-            projectService.UpdateProjectMiscWhenMemberUpdated(projectId,global_role_name);
+            projectService.UpdateProjectMiscWhenMemberUpdated(projectId,
+                                                              global_role_name);
         }
 
         return updateResult;
